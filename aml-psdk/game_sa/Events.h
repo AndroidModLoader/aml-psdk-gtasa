7f72ad73e1c892ebe41dc70ba55bb712b5529db5
RusJJ <mctesting1@yandex.ru>
2026-01-24 22:42:02 +0300

    Add more events

--- aml-psdk/game_sa/Events.h
+++ aml-psdk/game_sa/Events.h
@@ -1,7 +1,46 @@
 #include "plugin.h"
 #include <aml-psdk/sdk_callevents.h>
 
+struct CCamera;
+
 struct Events
 {
+    // PC psdk events
+    DECL_EVENT_PLT_ARG0(void, restartGameEvent, BYBIT(0x674E38, 0x848318));
+    DECL_EVENT_PLT_ARG0(void, drawingEvent, BYBIT(0x675CC4, 0x849AB0));
+    DECL_EVENT_PLT_ARG0(void, drawHudEvent, BYBIT(0x6737EC, 0x845EC8));
+    DECL_EVENT_PLT_ARG0(void, drawAfterFadeEvent, BYBIT(0x673C4C, 0x846618));
+    DECL_EVENT_SYM_ARG0(void, drawRadarEvent, _ZN4CHud9DrawRadarEv);
+    //DECL_EVENT_PLT_ARG0(void, drawBlipsEvent, BYBIT(0x66E910, 0x83DED0)); // arg0 float
+    //DECL_EVENT_SYM_ARG0(void, drawRadarOverlayEvent, _ZN6CRadar20DrawRadarGangOverlayEb); // arg0 bool
+    // drawMenuBackgroundEvent
+    DECL_EVENT_SYM_ARG0(void, initRwEvent, _ZN5CGame20InitialiseRenderWareEv);
+    DECL_EVENT_PLT_ARG0(void, shutdownRwEvent, BYBIT(0x6756F0, 0x849148));
+    // vehicleCtorEvent
+    // vehicleDtorEvent
+    // pedCtorEvent
+    // pedDtorEvent
+    // objectCtorEvent
+    // objectDtorEvent
+    DECL_EVENT_PLT_ARG0(void, initPoolsEvent, BYBIT(0x672468, 0x843F18));
+    DECL_EVENT_PLT_ARG0(void, shutdownPoolsEvent, BYBIT(0x66F218, 0x83EDC0));
+    // vehicleRenderEvent
+    // pedRenderEvent
+    // objectRenderEvent
+    // vehicleSetModelEvent
+    // pedSetModelEvent
+    // d3dResetEvent // ogl analogue
+    // d3dLostEvent // do we need this?
     DECL_EVENT_SYM_ARG0(void, gameProcessEvent, _ZN5CGame7ProcessEv);
+    DECL_EVENT_PLT_ARG0(void, initGameEvent, BYBIT(0x6740A4, 0x846D20));
+    DECL_EVENT_PLT_ARG0(void, reInitGameEvent, BYBIT(0x672014, 0x843810));
+    // onPauseAllSounds
+    // onResumeAllSounds
+    DECL_EVENT_PLT_ARG0(void, initScriptsEvent, BYBIT(0x671B14, 0x842FC0));
+    DECL_EVENT_PLT_ARG0(void, processScriptsEvent, BYBIT(0x673178, 0x845438)); // wrong on PC. bruh...
+    // menuDrawingEvent
+    DECL_EVENT_SYM_ARG0(bool, attachRwPluginsEvent, _Z12PluginAttachv);
+
+    // Additional events
+    DECL_EVENT_PLT_ARG1(void, processCamera, BYBIT(0x6717BC, 0x8429D8), CCamera*, camera);
 };
--- aml-psdk/sdk_callevents.h
+++ aml-psdk/sdk_callevents.h
@@ -27,6 +27,25 @@
             } \
         }
 
+#define DECL_EVENT_PLT(_ret, _addr, ...) \
+        typedef _ret (MyType)(__VA_ARGS__); \
+        static inline MyType* m_pOriginalFunc = NULL; \
+        struct { \
+            std::vector<MyType*> m_list; \
+            void operator+=(MyType* newFn) { \
+                m_list.push_back(newFn); StartEvent(); \
+            } \
+            void operator-=(MyType* newFn) { \
+                for(auto it = m_list.begin(); it != m_list.end(); ++it) { if(*it == newFn) { m_list.erase(it); break; } } \
+            } \
+        } before, after; \
+        void operator+=(MyType* newFn) { after += newFn; } \
+        static inline void StartEvent() { \
+            if(!m_pOriginalFunc) { \
+                aml->HookPLT((void*)(GetMainLibraryAddress() + _addr), (void*)(&EventExecuted), (void**)(&m_pOriginalFunc)); \
+            } \
+        }
+
 #define DECL_EVENT_BASE_END(_name) \
     }; static inline CallEvent_##_name _name
 
@@ -56,9 +75,56 @@
         } \
     DECL_EVENT_BASE_END(_name)
 
-#define DECL_EVENT_SYM_ARG1(_ret, _name, _sym, _t1, _v1) \
+#define DECL_EVENT_PLT_ARG0(_ret, _name, _addr) \
     DECL_EVENT_BASE(_name) \
-        DECL_EVENT_SYM(_ret, _sym, _t1 _v1) \
+        DECL_EVENT_PLT(_ret, _addr) \
+        inline void CallBefore() { \
+            for(auto fn : before.m_list) fn(); \
+        } \
+        inline void CallAfter() { \
+            for(auto fn : after.m_list) fn(); \
+        } \
+        template<typename RetType = _ret> \
+        static inline RetType EventExecutedImpl() { \
+            _name.CallBefore(); \
+            if constexpr (std::is_same_v<RetType, void>) { \
+                _name.m_pOriginalFunc(); \
+                _name.CallAfter(); \
+            } else { \
+                RetType ret = _name.m_pOriginalFunc(); \
+                _name.CallAfter(); \
+                return ret; \
+            } \
+        } \
+        static inline _ret EventExecuted() { \
+            return EventExecutedImpl(); \
+        } \
+    DECL_EVENT_BASE_END(_name)
+
+#define DECL_EVENT_PLT_ARG1(_ret, _name, _addr, _t1, _v1) \
+    DECL_EVENT_BASE(_name) \
+        DECL_EVENT_PLT(_ret, _addr, _t1 _v1) \
+        inline void CallBefore(_t1 _v1) { \
+            for(auto fn : before.m_list) fn(_v1); \
+        } \
+        inline void CallAfter(_t1 _v1) { \
+            for(auto fn : after.m_list) fn(_v1); \
+        } \
+        template<typename RetType = _ret> \
+        static inline RetType EventExecutedImpl(_t1 _v1) { \
+            _name.CallBefore(_v1); \
+            if constexpr (std::is_same_v<RetType, void>) { \
+                _name.m_pOriginalFunc(_v1); \
+                _name.CallAfter(_v1); \
+            } else { \
+                RetType ret = _name.m_pOriginalFunc(_v1); \
+                _name.CallAfter(_v1); \
+                return ret; \
+            } \
+        } \
+        static inline _ret EventExecuted(_t1 _v1) { \
+            return EventExecutedImpl(_v1); \
+        } \
     DECL_EVENT_BASE_END(_name)
 
 #endif // __AML_PSDK_CALLEVENTS_H
