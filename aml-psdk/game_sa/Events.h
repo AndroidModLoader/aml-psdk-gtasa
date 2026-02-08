8a38e357d415b2cfef9177b7faa509733edb8134
RusJJ <mctesting1@yandex.ru>
2026-02-08 22:55:09 +0300

    CWorld

--- aml-psdk/game_sa/Events.h
+++ aml-psdk/game_sa/Events.h
@@ -11,7 +11,7 @@ struct MobileMenu;
 
 struct Events
 {
-    // PC psdk events
+    // PC psdk events (they are slightly different from PC. Please keep that in mind!)
     DECL_EVENT_PLT_ARG0_PICK0(void, restartGameEvent, BYBIT(0x674E38, 0x848318));
     DECL_EVENT_PLT_ARG0_PICK0(void, drawingEvent, BYBIT(0x675CC4, 0x849AB0));
     DECL_EVENT_PLT_ARG0_PICK0(void, drawHudEvent, BYBIT(0x6737EC, 0x845EC8));
@@ -26,13 +26,13 @@ struct Events
     DECL_EVENT_SYM_ARG1_PICK1(void, vehicleDtorEvent, _ZN8CVehicleD2Ev, CVehicle*, vehicle);
     DECL_EVENT_SYM_ARG2_PICK1(void, pedCtorEvent, _ZN4CPedC2Ej, CPed*, ped, u32, type);
     DECL_EVENT_SYM_ARG1_PICK1(void, pedDtorEvent, _ZN4CPedD2Ev, CPed*, ped);
-    // objectCtorEvent
-    // objectDtorEvent
+    DECL_EVENT_SYM_ARG1_PICK1(void, objectCtorEvent, _ZN7CObject4InitEv, CObject*, object);
+    DECL_EVENT_SYM_ARG1_PICK1(void, objectDtorEvent, _ZN7CObjectD2Ev, CObject*, object);
     DECL_EVENT_PLT_ARG0_PICK0(void, initPoolsEvent, BYBIT(0x672468, 0x843F18));
     DECL_EVENT_PLT_ARG0_PICK0(void, shutdownPoolsEvent, BYBIT(0x66F218, 0x83EDC0));
-    // vehicleRenderEvent
-    // pedRenderEvent
-    // objectRenderEvent
+    DECL_EVENT_SYM_ARG1_PICK1(void, vehicleRenderEvent, _ZN8CVehicle6RenderEv, CVehicle*, vehicle);
+    DECL_EVENT_SYM_ARG1_PICK1(void, pedRenderEvent, _ZN4CPed6RenderEv, CPed*, ped);
+    DECL_EVENT_SYM_ARG1_PICK1(void, objectRenderEvent, _ZN7CObject6RenderEv, CObject*, object);
     DECL_EVENT_SYM_ARG2_PICK2(void, vehicleSetModelEvent, _ZN8CVehicle13SetModelIndexEj, CVehicle*, vehicle, int, model);
     DECL_EVENT_SYM_ARG2_PICK2(void, pedSetModelEvent, _ZN4CPed13SetModelIndexEj, CVehicle*, vehicle, int, model);
     // d3dResetEvent // ogl analogue
@@ -47,7 +47,7 @@ struct Events
     DECL_EVENT_PLT_ARG1_PICK0(void, menuDrawingEvent, BYBIT(0x674254, 0x846FE8), MobileMenu*, mobilemenu);
     DECL_EVENT_SYM_ARG0_PICK0(bool, attachRwPluginsEvent, _Z12PluginAttachv);
 
-    // Additional events
+    // Additional events (aml psdk)
     DECL_EVENT_PLT_ARG1_PICK1(void, processCamera, BYBIT(0x6717BC, 0x8429D8), CCamera*, camera);
 };
 
